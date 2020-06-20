193eed0fad28b49b44ea15ee9dc68b9eec5bf5bf
Rom858 <65760336+Rom858@users.noreply.github.com>
2020-06-20 15:54:49 +0700

    Optional paramater
    Default

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,19 @@
 
 // };
 // console.log(greetUnnamed('rom',18))
-function add(foo: number, bar: number, foobar: number): number {
+// ======================================
+// Defualt parameter have 2 method to use it
+// 1--hard to understant
+function add(foo: number, bar: number, foobar?: number): number {
+  return foo + bar + (foobar !== undefined ? foobar : 5);
+}
+// 2--code is readable
+function add(foo: number, bar: number, foobar: number = 0): number {
   return foo + bar + foobar;
 }
-console.log(add(1, 2, 3));
+console.log(add(1, 2, 8));
 add(2, 2); // Error, expected 3 arguments, but got 2.
-add(2, 2, 2); // OK, returns 6
+add(2, 2, 2); // OK, returns 6d
+
+// parameter foobar?: number mean have or dont have its ok for this parameter
+//============================
