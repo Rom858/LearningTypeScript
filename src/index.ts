39ba4abcb78c74914962f96756cf81919124a26d
Rom858 <65760336+Rom858@users.noreply.github.com>
2020-06-20 15:32:40 +0700

    TSX
    TypeFunc

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
-document.getElementById("app").innerHTML = `
-<h1>Hello Parcel!</h1>
-<div>
-  Look
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>
-  for more info about Parcel.
-</div>
-`;
+// let greetUnnamed = function(name: string,age:number,): string {
+// return `Hi! Age: ${age}`;
+
+// };
+// console.log(greetUnnamed('rom',18))
+function add(foo: number, bar: number, foobar: number): number {
+  return foo + bar + foobar;
+}
+console.log(add(1, 2, 3));
+add(2, 2); // Error, expected 3 arguments, but got 2.
+add(2, 2, 2); // OK, returns 6
